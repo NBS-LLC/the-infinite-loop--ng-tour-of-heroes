96b82675b5cdf096a8d43a52e4064c2e715078b6
Network Based Solutions, LLC <35822984+NBS-LLC@users.noreply.github.com>
2022-07-15 01:41:19 -0700

    Ported Heroes E2E Tests (#6)
    * Removed ported code.
    * Organized getPageElts() returned object's properties.
    * Finished porting Heroes tests.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,9 @@
-import { browser, element, by, ElementFinder, ElementArrayFinder } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 const targetHero = { id: 15, name: 'Magneta' };
-const nameSuffix = 'X';
-const newHeroName = targetHero.name + nameSuffix;
 
 class Hero {
-  constructor(public id: number, public name: string) {}
+  constructor(public id: number, public name: string) { }
 
   // Factory methods
 
@@ -63,93 +61,6 @@ describe('Tutorial part 6', () => {
     };
   }
 
-  describe('Heroes tests', () => {
-
-    beforeAll(() => browser.get(''));
-
-    it('can switch to Heroes view', async () => {
-      await getPageElts().appHeroesHref.click();
-      const page = getPageElts();
-      expect(await page.appHeroes.isPresent()).toBeTruthy();
-      expect(await page.allHeroes.count()).toEqual(9, 'number of heroes');
-    });
-
-    it('can route to hero details', async () => {
-      await getHeroLiEltById(targetHero.id).click();
-
-      const page = getPageElts();
-      expect(await page.heroDetail.isPresent()).toBeTruthy('shows hero detail');
-      const hero = await Hero.fromDetail(page.heroDetail);
-      expect(hero.id).toEqual(targetHero.id);
-      expect(hero.name).toEqual(targetHero.name.toUpperCase());
-    });
-
-    it(`updates hero name (${newHeroName}) in details view`, updateHeroNameInDetailView);
-
-    it(`shows ${newHeroName} in Heroes list`, async () => {
-      await element(by.buttonText('save')).click();
-      await browser.waitForAngular();
-      const expectedText = `${targetHero.id} ${newHeroName}`;
-      expect(await getHeroAEltById(targetHero.id).getText()).toEqual(expectedText);
-    });
-
-    it(`deletes ${newHeroName} from Heroes list`, async () => {
-      const heroesBefore = await toHeroArray(getPageElts().allHeroes);
-      const li = getHeroLiEltById(targetHero.id);
-      await li.element(by.buttonText('x')).click();
-
-      const page = getPageElts();
-      expect(await page.appHeroes.isPresent()).toBeTruthy();
-      expect(await page.allHeroes.count()).toEqual(8, 'number of heroes');
-      const heroesAfter = await toHeroArray(page.allHeroes);
-      // console.log(await Hero.fromLi(page.allHeroes[0]));
-      const expectedHeroes =  heroesBefore.filter(h => h.name !== newHeroName);
-      expect(heroesAfter).toEqual(expectedHeroes);
-      // expect(page.selectedHeroSubview.isPresent()).toBeFalsy();
-    });
-
-    it(`adds back ${targetHero.name}`, async () => {
-      const addedHeroName = 'Alice';
-      const heroesBefore = await toHeroArray(getPageElts().allHeroes);
-      const numHeroes = heroesBefore.length;
-
-      await element(by.css('input')).sendKeys(addedHeroName);
-      await element(by.buttonText('Add hero')).click();
-
-      const page = getPageElts();
-      const heroesAfter = await toHeroArray(page.allHeroes);
-      expect(heroesAfter.length).toEqual(numHeroes + 1, 'number of heroes');
-
-      expect(heroesAfter.slice(0, numHeroes)).toEqual(heroesBefore, 'Old heroes are still there');
-
-      const maxId = heroesBefore[heroesBefore.length - 1].id;
-      expect(heroesAfter[numHeroes]).toEqual({id: maxId + 1, name: addedHeroName});
-    });
-
-    it('displays correctly styled buttons', async () => {
-      const buttons = await element.all(by.buttonText('x'));
-
-      for (const button of buttons) {
-        // Inherited styles from styles.css
-        expect(await button.getCssValue('font-family')).toBe('Arial, Helvetica, sans-serif');
-        expect(await button.getCssValue('border')).toContain('none');
-        expect(await button.getCssValue('padding')).toBe('1px 10px 3px');
-        expect(await button.getCssValue('border-radius')).toBe('4px');
-        // Styles defined in heroes.component.css
-        expect(await button.getCssValue('left')).toBe('210px');
-        expect(await button.getCssValue('top')).toBe('5px');
-      }
-
-      const addButton = element(by.buttonText('Add hero'));
-      // Inherited styles from styles.css
-      expect(await addButton.getCssValue('font-family')).toBe('Arial, Helvetica, sans-serif');
-      expect(await addButton.getCssValue('border')).toContain('none');
-      expect(await addButton.getCssValue('padding')).toBe('8px 24px');
-      expect(await addButton.getCssValue('border-radius')).toBe('4px');
-    });
-
-  });
-
   describe('Progressive hero search', () => {
 
     beforeAll(() => browser.get(''));
@@ -188,34 +99,4 @@ describe('Tutorial part 6', () => {
       expect(hero2.name).toEqual(targetHero.name.toUpperCase());
     });
   });
-
-  async function updateHeroNameInDetailView() {
-    // Assumes that the current view is the hero details view.
-    await addToHeroName(nameSuffix);
-
-    const page = getPageElts();
-    const hero = await Hero.fromDetail(page.heroDetail);
-    expect(hero.id).toEqual(targetHero.id);
-    expect(hero.name).toEqual(newHeroName.toUpperCase());
-  }
-
 });
-
-async function addToHeroName(text: string): Promise<void> {
-  const input = element(by.css('input'));
-  await input.sendKeys(text);
-}
-
-function getHeroAEltById(id: number): ElementFinder {
-  const spanForId = element(by.cssContainingText('li span.badge', id.toString()));
-  return spanForId.element(by.xpath('..'));
-}
-
-function getHeroLiEltById(id: number): ElementFinder {
-  const spanForId = element(by.cssContainingText('li span.badge', id.toString()));
-  return spanForId.element(by.xpath('../..'));
-}
-
-async function toHeroArray(allHeroes: ElementArrayFinder): Promise<Hero[]> {
-  return allHeroes.map(hero => Hero.fromLi(hero!));
-}
