b2675368212f0576723d4bc2bbe10040c0382539
NBS-LLC <nick@networkbasedsolutions.com>
2022-10-31 18:28:30 -0700

    Added a Custom Locator to WDIO
    * Removed unnecessary reference to the browser object.

--- e2e/test/specs/app.ts
+++ e2e/test/specs/app.ts
@@ -17,7 +17,7 @@ function getPageElts() {
         heroDetail: $('app-root app-hero-detail > div'),
 
         searchBox: $('#search-box'),
-        searchResults: browser.$$('testid=search-result-item')
+        searchResults: $$('testid=search-result-item')
     };
 }
 
